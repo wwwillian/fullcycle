0515ccb1b6de78159b1437d9d0d157140653b850
wwwillian <contato@wwwillian.com>
2021-09-27 18:01:39 -0300

    Implamentando DDD

---
+++ arquitetura/hexagonal/adapters/web/server/serve.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/wwwillian/go-hexagonal/adapters/web/handler"
+	"github.com/wwwillian/go-hexagonal/application"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Webserver struct {
+	Service application.ProductServiceInterface
+}
+
+func MakeNewWebserver() *Webserver {
+	return &Webserver{}
+}
+
+func (w Webserver) Serve() {
+	r := mux.NewRouter()
+	n := negroni.New(
+			negroni.NewLogger(),
+		)
+
+	handler.MakeProductHandlers(r, n, w.Service)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		ReadHeaderTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		Addr: ":9000",
+		Handler: http.DefaultServeMux,
+		ErrorLog: log.New(os.Stderr, "log: ", log.Lshortfile),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ arquitetura/hexagonal/application/product_test.go
@@ -0,0 +1,103 @@
+package application_test
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/wwwillian/go-hexagonal/application"
+	"testing"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price =  -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+}
+
+func TestProduct_GetID(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	require.Equal(t, product.GetID(), product.ID)
+}
+
+func TestProduct_GetName(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	require.Equal(t, product.GetName(), product.Name)
+}
+
+func TestProduct_GetPrice(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	require.Equal(t, product.GetPrice(), product.Price)
+}
+
+func TestProduct_GetStatus(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	require.Equal(t, product.GetStatus(), product.Status)
+
+	product.Status = application.ENABLED
+	product.Price = 0
+	require.Equal(t, product.GetStatus(), product.Status)
+}
